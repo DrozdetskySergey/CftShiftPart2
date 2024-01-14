1ce50b22331575b3ae11e97235b89f6e220e487a
DrozdetskySergey <master@2867073.ru>
2024-01-14 20:17:52 +0700

    add FileWriterFromBuffer

---
+++ FilesFilter/src/ru/cft/drozdetskiy/FileWriterFromBuffer.java
@@ -0,0 +1,29 @@
+package ru.cft.drozdetskiy;
+
+import ru.cft.drozdetskiy.buffer.Buffer;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+class FileWriterFromBuffer<T> {
+
+    private final String file;
+    private final Buffer<T> buffer;
+
+    public FileWriterFromBuffer(String file, Buffer<T> buffer) {
+        this.file = file;
+        this.buffer = buffer;
+    }
+
+    public void run() {
+        try (FileWriter writer = new FileWriter(file, true);
+             BufferedWriter bufferWriter = new BufferedWriter(writer)) {
+            for (T value = buffer.get(); value != null; value = buffer.get()) {
+                bufferWriter.write(value.toString());
+            }
+        } catch (IOException e) {
+            System.out.printf("Сбой записи в файл: %s%n", file);
+        }
+    }
+}
