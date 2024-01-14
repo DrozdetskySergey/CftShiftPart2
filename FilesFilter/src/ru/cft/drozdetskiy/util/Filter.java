157132ac122c02c1c80986037d292d54df214743
DrozdetskySergey <master@2867073.ru>
2024-01-14 18:52:04 +0700

    add ulil Filter and FilterType

---
+++ FilesFilter/src/ru/cft/drozdetskiy/util/Filter.java
@@ -0,0 +1,34 @@
+package ru.cft.drozdetskiy.util;
+
+import ru.cft.drozdetskiy.buffer.Buffer;
+import ru.cft.drozdetskiy.supplier.StringSupplier;
+
+public class Filter {
+
+    public static void divide(StringSupplier supplier,
+                              Buffer<Integer> integerBuffer,
+                              Buffer<Double> doubleBuffer,
+                              Buffer<String> stringBuffer) {
+        for (String s = supplier.next(); s != null; s = supplier.next()) {
+            FilterType type = FilterType.STRING;
+            Double number = null;
+            Integer integer = null;
+
+            try {
+                number = Double.valueOf(s);
+                type = FilterType.DOUBLE;
+                integer = Integer.valueOf(s);
+                type = FilterType.INTEGER;
+            } catch (NumberFormatException ignored) {
+            }
+
+            if (type == FilterType.INTEGER) {
+                integerBuffer.add(integer);
+            } else if (type == FilterType.DOUBLE) {
+                doubleBuffer.add(number);
+            } else {
+                stringBuffer.add(s);
+            }
+        }
+    }
+}
---
+++ FilesFilter/src/ru/cft/drozdetskiy/util/FilterType.java
@@ -0,0 +1,6 @@
+package ru.cft.drozdetskiy.util;
+
+public enum FilterType {
+
+    INTEGER, DOUBLE, STRING
+}
