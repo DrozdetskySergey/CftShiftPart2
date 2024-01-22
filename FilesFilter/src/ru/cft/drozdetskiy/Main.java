1d7186c28fd786ba753199ff413136456875d585
DrozdetskySergey <master@2867073.ru>
2024-01-23 00:38:34 +0700

    update main (correct folder)

--- FilesFilter/src/ru/cft/drozdetskiy/Main.java
+++ FilesFilter/src/ru/cft/drozdetskiy/Main.java
@@ -2,7 +2,6 @@ package ru.cft.drozdetskiy;
 
 import ru.cft.drozdetskiy.buffer.Buffer;
 import ru.cft.drozdetskiy.buffer.impl.FastBuffer;
-import ru.cft.drozdetskiy.statistics.StatisticsException;
 import ru.cft.drozdetskiy.statistics.StatisticsFactory;
 import ru.cft.drozdetskiy.statistics.StatisticsFactoryBuilder;
 import ru.cft.drozdetskiy.statistics.StatisticsType;
@@ -10,6 +9,9 @@ import ru.cft.drozdetskiy.supplier.StringSupplier;
 import ru.cft.drozdetskiy.supplier.impl.FileStringSupplier;
 import ru.cft.drozdetskiy.writer.FileWriterFromBuffer;
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
 
@@ -18,14 +20,15 @@ public class Main {
     public static void main(String[] args) {
         List<String> inputFiles = Arrays.asList("in1", "in2", "in3");
         StatisticsType statisticsType = StatisticsType.FULL;
-        boolean isAppend = false;
-        String folderPlusPrefix = "";
+        boolean isAppend = true;
+        String prefix = "";
+        String folder = "";
 
         try (StringSupplier supplier = new FileStringSupplier(inputFiles)) {
-            StatisticsFactory factory = StatisticsFactoryBuilder.buildFactory(statisticsType);
-            Buffer<Long> longBuffer = new FastBuffer<>(factory.createForLong());
-            Buffer<Double> doubleBuffer = new FastBuffer<>(factory.createForDouble());
-            Buffer<String> stringBuffer = new FastBuffer<>(factory.createForString());
+            StatisticsFactory statisticsFactory = StatisticsFactoryBuilder.buildFactory(statisticsType);
+            Buffer<Long> longBuffer = new FastBuffer<>(statisticsFactory.createForLong());
+            Buffer<Double> doubleBuffer = new FastBuffer<>(statisticsFactory.createForDouble());
+            Buffer<String> stringBuffer = new FastBuffer<>(statisticsFactory.createForString());
 
             Filter.divide(supplier, longBuffer, doubleBuffer, stringBuffer);
 
@@ -33,27 +36,33 @@ public class Main {
             System.out.println(doubleBuffer.getStatisticsInfo());
             System.out.println(stringBuffer.getStatisticsInfo());
 
+            folder += "\\";
+
+            if (!Paths.get(folder).isAbsolute()) {
+                folder = Paths.get("").toAbsolutePath().toString() + "\\" + folder;
+            }
+
+            Files.createDirectories(Paths.get(folder));
+
             if (longBuffer.size() > 0) {
-                String longFile = folderPlusPrefix + "integers.txt";
+                Path longFile = Paths.get(folder + prefix + "integers.txt");
                 FileWriterFromBuffer<Long> longWriter = new FileWriterFromBuffer<>(longFile, longBuffer, isAppend);
                 longWriter.run();
             }
 
             if (doubleBuffer.size() > 0) {
-                String doubleFile = folderPlusPrefix + "floats.txt";
+                Path doubleFile = Paths.get(folder + prefix + "floats.txt");
                 FileWriterFromBuffer<Double> doubleWriter = new FileWriterFromBuffer<>(doubleFile, doubleBuffer, isAppend);
                 doubleWriter.run();
             }
 
             if (stringBuffer.size() > 0) {
-                String stringFile = folderPlusPrefix + "strings.txt";
+                Path stringFile = Paths.get(folder + prefix + "strings.txt");
                 FileWriterFromBuffer<String> stringWriter = new FileWriterFromBuffer<>(stringFile, stringBuffer, isAppend);
                 stringWriter.run();
             }
-        } catch (StatisticsException e) {
-            System.out.println(e.getMessage());
         } catch (Exception e) {
-            e.printStackTrace();
+            System.out.println(e.getMessage());
         }
     }
 }
