6e787a7dbd5dd68dfd76ff3013a1c341b4c8f700
DrozdetskySergey <master@2867073.ru>
2024-01-23 18:54:20 +0700

    add class Parser (arguments)

---
+++ FilesFilter/src/ru/cft/drozdetskiy/args/Parser.java
@@ -0,0 +1,100 @@
+package ru.cft.drozdetskiy.args;
+
+import ru.cft.drozdetskiy.statistics.StatisticsType;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+import static ru.cft.drozdetskiy.args.Key.*;
+
+public class Parser {
+
+    private final List<String> inputFiles = new ArrayList<>();
+    private StatisticsType statisticsType = StatisticsType.SIMPLE;
+    private boolean isAppend = false;
+    private String prefix = "";
+    private String folder = "";
+
+    public Parser(String[] args) {
+        List<String> arguments = decompose(filter(args));
+
+        for (int i = 0; i < arguments.size(); i++) {
+            if (arguments.get(i).startsWith("-")) {
+                char key = arguments.get(i).charAt(1);
+
+                if (key == APPEND_FILES.symbol) {
+                    isAppend = true;
+                } else if (key == SIMPLE_STAT.symbol) {
+                    statisticsType = StatisticsType.SIMPLE;
+                } else if (key == FULL_STAT.symbol) {
+                    statisticsType = StatisticsType.FULL;
+                } else if (key == SET_FOLDER.symbol && i < arguments.size() - 1 && !arguments.get(i + 1).startsWith("-")) {
+                    folder = arguments.get(++i);
+                } else if (key == SET_PREFIX.symbol && i < arguments.size() - 1 && !arguments.get(i + 1).startsWith("-")) {
+                    prefix = arguments.get(++i);
+                } else {
+                    System.out.printf("Не известный параметр: %c%n", key);
+                }
+            } else {
+                inputFiles.add(arguments.get(i));
+            }
+        }
+    }
+
+    public List<String> getInputFiles() {
+        return inputFiles;
+    }
+
+    public StatisticsType getStatisticsType() {
+        return statisticsType;
+    }
+
+    public boolean isAppend() {
+        return isAppend;
+    }
+
+    public String getPrefix() {
+        return prefix;
+    }
+
+    public String getFolder() {
+        return folder;
+    }
+
+    private List<String> filter(String[] args) {
+        return Arrays.stream(args)
+                .filter(Objects::nonNull)
+                .filter(Predicate.not(String::isBlank))
+                .map(String::strip)
+                .collect(Collectors.toList());
+    }
+
+    private List<String> decompose(List<String> arguments) {
+        List<String> result = new ArrayList<>();
+
+        for (String s : arguments) {
+            if (s.startsWith("-")) {
+                char[] symbols = s.substring(1).toCharArray();
+
+                for (int i = 0; i < symbols.length; i++) {
+                    result.add("-" + symbols[i]);
+
+                    if (symbols[i] == SET_FOLDER.symbol || symbols[i] == SET_PREFIX.symbol) {
+                        if (i < symbols.length - 1) {
+                            result.add(s.substring(i + 1));
+                            break;
+                        }
+                    }
+                }
+            } else {
+                result.add(s);
+            }
+        }
+
+        return result;
+    }
+}
