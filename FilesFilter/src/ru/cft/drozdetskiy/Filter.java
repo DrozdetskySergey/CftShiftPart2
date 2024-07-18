a73222f36d1ba8910b03cdf198cd35892e92c0a3
DrozdetskySergey <master@2867073.ru>
2024-07-18 23:27:41 +0700

    refact Filter

--- FilesFilter/src/ru/cft/drozdetskiy/Filter.java
+++ FilesFilter/src/ru/cft/drozdetskiy/Filter.java
@@ -11,14 +11,12 @@ class Filter {
                               Buffer<Double> doubleBuffer,
                               Buffer<String> stringBuffer) {
         for (String s = supplier.next(); s != null; s = supplier.next()) {
-            if (NumberUtils.isCreatable(s)) {
-                if (isLongInteger(s)) {
-                    longBuffer.add(Long.valueOf(s));
-                } else if (isDecimalSystem(s)) {
-                    doubleBuffer.add(Double.valueOf(s));
-                } else {
-                    stringBuffer.add(s);
-                }
+            if (s.isBlank()) {
+                stringBuffer.add(s);
+            } else if (isLongInteger(s)) {
+                longBuffer.add(Long.valueOf(s));
+            } else if (isDecimalSystem(s) && NumberUtils.isCreatable(s)) {
+                doubleBuffer.add(Double.valueOf(s));
             } else {
                 stringBuffer.add(s);
             }
@@ -28,7 +26,6 @@ class Filter {
     private static boolean isDecimalSystem(String string) {
         for (char c : string.toCharArray()) {
             if ((c < '0' || c > '9') && c != '.' && c != '-' && c != '+' && c != 'e' && c != 'E') {
-
                 return false;
             }
         }
@@ -37,35 +34,30 @@ class Filter {
     }
 
     private static boolean isLongInteger(String string) {
-        int i = string.charAt(0) == '-' || string.charAt(0) == '+' ? 1 : 0;
+        char firstSymbol = string.charAt(0);
+        int i = firstSymbol == '-' || firstSymbol == '+' ? 1 : 0;
         int length = string.length();
-        boolean isNeededHardCheck = false;
+        boolean isNeededDeepVerification = false;
 
-        if (length - i > 18) {
-            if (length - i > 19) {
-
-                return false;
-            } else if (string.charAt(i) == '9') {
-                isNeededHardCheck = true;
-            }
+        if (length - i < 1 || length - i > 19) {
+            return false;
+        } else if (length - i == 19 && string.charAt(i) == '9') {
+            isNeededDeepVerification = true;
         }
 
         for (; i < length; i++) {
             char symbol = string.charAt(i);
 
             if (symbol < '0' || symbol > '9') {
-
                 return false;
             }
         }
 
-        if (isNeededHardCheck) {
-            try {
-                Long.valueOf(string);
-            } catch (NumberFormatException e) {
+        if (isNeededDeepVerification) {
+            String digits = firstSymbol == '-' || firstSymbol == '+' ? string.substring(1) : string;
 
-                return false;
-            }
+            return (firstSymbol != '-' || digits.compareTo("9223372036854775808") <= 0) &&
+                    (firstSymbol == '-' || digits.compareTo("9223372036854775807") <= 0);
         }
 
         return true;
