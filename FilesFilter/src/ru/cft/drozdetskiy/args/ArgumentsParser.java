1ff31ef5e113749ce3b96683057957d83fa8c10a
DrozdetskySergey <master@2867073.ru>
2025-02-21 22:38:40 +0700

    update javadoc ArgumentsParser class

--- FilesFilter/src/ru/cft/drozdetskiy/args/ArgumentsParser.java
+++ FilesFilter/src/ru/cft/drozdetskiy/args/ArgumentsParser.java
@@ -8,11 +8,21 @@ import java.util.stream.Collectors;
 
 import static ru.cft.drozdetskiy.args.Option.*;
 
+/**
+ * Функциональный класс. Предоставляет статичный метод parse.
+ */
 public final class ArgumentsParser {
 
     private ArgumentsParser() {
     }
 
+    /**
+     * Парсит массив строк, распознает опции {@link Option}, имена файлов и другие данный.
+     *
+     * @param args Массив строк.
+     * @return DTO с нужными даннами для программы.
+     * @throws IllegalArgumentException если встречается не известная опция.
+     */
     public static ArgumentsDTO parse(String[] args) {
         StringBuilder prefix = new StringBuilder();
         StringBuilder folder = new StringBuilder();
@@ -54,10 +64,22 @@ public final class ArgumentsParser {
                 .build();
     }
 
+    /**
+     * Проверяет строка (аргумент) это опция или нет (начинается с минуса или нет).
+     *
+     * @param string Проверяемый аргумент.
+     * @return true если это не опция или false если опция.
+     */
     private static boolean isNotOption(String string) {
         return !string.startsWith("-");
     }
 
+    /**
+     * Отфильтровывает null и пустые строки, убирает пробелы в начале и в конце каждой стоки.
+     *
+     * @param args Массив сток.
+     * @return Список релевантных строк.
+     */
     private static List<String> filter(String[] args) {
         return Arrays.stream(args)
                 .filter(Objects::nonNull)
@@ -66,6 +88,12 @@ public final class ArgumentsParser {
                 .collect(Collectors.toList());
     }
 
+    /**
+     * Не опции копирует без изменения. Составные (слипшиеся) опции разбивает на одиночные.
+     *
+     * @param arguments Список аргументов.
+     * @return Список аргументов без составных опций.
+     */
     private static List<String> decompose(List<String> arguments) {
         List<String> result = new ArrayList<>();
 
