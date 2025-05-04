cc159dffcf75ae81c3745fa6f271f67882b3c08d
DrozdetskySergey <master@2867073.ru>
2025-05-04 18:30:16 +0700

    refact LazyWriter, javadoc

--- FilesFilter/src/ru/cft/drozdetskiy/LazyWriter.java
+++ FilesFilter/src/ru/cft/drozdetskiy/LazyWriter.java
@@ -8,6 +8,10 @@ import java.nio.file.OpenOption;
 import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 
+/**
+ * Proxy класс реализующий интерфейс Appendable.
+ * Создаёт проксируемый объект класса BufferedWriter только тогда, когда первый раз вызывается метод интерфейса.
+ */
 final class LazyWriter implements Appendable, Closeable {
 
     private static final OpenOption[] WRITE_OPTIONS =
@@ -19,6 +23,12 @@ final class LazyWriter implements Appendable, Closeable {
     private final OpenOption[] openOptions;
     private Writer writer;
 
+    /**
+     * Proxy класс реализующий интерфейс Appendable. Проксируемый объект - writer в файл.
+     *
+     * @param file     путь к файлу.
+     * @param isAppend true если способ открытия файла для записи в конец, false если перезапись файла.
+     */
     public LazyWriter(Path file, boolean isAppend) {
         this.file = file;
         openOptions = isAppend ? APPEND_OPTIONS : WRITE_OPTIONS;
@@ -52,6 +62,12 @@ final class LazyWriter implements Appendable, Closeable {
         }
     }
 
+    /**
+     * Создаёт объект writer если он ещё не существует.
+     *
+     * @return ссылка на объект writer.
+     * @throws IOException если ошибка создания или открытия файла.
+     */
     private Writer getWriter() throws IOException {
         if (writer == null) {
             writer = Files.newBufferedWriter(file, openOptions);
