e7c9ca2806f65382a357a1969770ee825837ed75
DrozdetskySergey <master@2867073.ru>
2025-05-10 18:07:37 +0700

    add FullStatisticsFactory javadoc

--- FilesFilter/src/ru/cft/drozdetskiy/statistics/impl/FullStatisticsFactory.java
+++ FilesFilter/src/ru/cft/drozdetskiy/statistics/impl/FullStatisticsFactory.java
@@ -3,6 +3,10 @@ package ru.cft.drozdetskiy.statistics.impl;
 import ru.cft.drozdetskiy.statistics.Statistics;
 import ru.cft.drozdetskiy.statistics.StatisticsFactory;
 
+/**
+ * Фабрика для создания объектов интерфейса {@link Statistics} параметризованных типами: {@link Long}, {@link Double}, {@link String}.
+ * Создаёт новые объекты для сбора полной (FULL) статистики.
+ */
 public final class FullStatisticsFactory implements StatisticsFactory {
 
     @Override
