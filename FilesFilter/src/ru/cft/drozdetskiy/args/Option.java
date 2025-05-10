c745881d1aca35ebb956cba81e920a858367804b
DrozdetskySergey <master@2867073.ru>
2025-05-10 19:23:07 +0700

    add Option javadoc

--- FilesFilter/src/ru/cft/drozdetskiy/args/Option.java
+++ FilesFilter/src/ru/cft/drozdetskiy/args/Option.java
@@ -1,11 +1,31 @@
 package ru.cft.drozdetskiy.args;
 
+/**
+ * Опции программы (аргументы начинающиеся с символа '-') переданные в main:
+ * {@linkplain #SET_DIRECTORY}, {@linkplain #SET_PREFIX}, {@linkplain #APPEND_FILES},
+ * {@linkplain #SIMPLE_STAT}, {@linkplain #FULL_STAT}
+ */
 enum Option {
 
+    /**
+     * Задать путь (директорию) для файлов с результатом. Синтаксис: -o путь
+     */
     SET_DIRECTORY('o'),
+    /**
+     * Задать префикс для имён файлов с результатом. Синтаксис: -p префикс
+     */
     SET_PREFIX('p'),
+    /**
+     * Включить режим записи в конец файла. Синтаксис: -a
+     */
     APPEND_FILES('a'),
+    /**
+     * Собирать краткую статистику. Синтаксис: -s
+     */
     SIMPLE_STAT('s'),
+    /**
+     * Собирать полную статистику. Синтаксис: -f
+     */
     FULL_STAT('f');
 
     final char symbol;
