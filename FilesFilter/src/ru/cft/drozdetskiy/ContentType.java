4754950d5cbc1446023a2357a333bf2783b981db
DrozdetskySergey <master@2867073.ru>
2025-05-10 19:33:17 +0700

    add ContentType javadoc

--- FilesFilter/src/ru/cft/drozdetskiy/ContentType.java
+++ FilesFilter/src/ru/cft/drozdetskiy/ContentType.java
@@ -1,5 +1,20 @@
 package ru.cft.drozdetskiy;
 
+/**
+ * Тип содержимого строки т.е. в какой объект можно распарсить всю строку:
+ * {@linkplain #LONG}, {@linkplain #DOUBLE}, {@linkplain #STRING}
+ */
 enum ContentType {
-    LONG, DOUBLE, STRING
+    /**
+     * Целое число.
+     */
+    LONG,
+    /**
+     * Вещественное число.
+     */
+    DOUBLE,
+    /**
+     * Строка.
+     */
+    STRING
 }
