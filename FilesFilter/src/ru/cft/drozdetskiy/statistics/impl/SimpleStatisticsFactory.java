ff0f3dbcb5c4539ae81a4471cd33b2c5587c4849
DrozdetskySergey <master@2867073.ru>
2025-05-10 18:09:16 +0700

    add SimpleStatisticsFactory javadoc

--- FilesFilter/src/ru/cft/drozdetskiy/statistics/impl/SimpleStatisticsFactory.java
+++ FilesFilter/src/ru/cft/drozdetskiy/statistics/impl/SimpleStatisticsFactory.java
@@ -3,6 +3,10 @@ package ru.cft.drozdetskiy.statistics.impl;
 import ru.cft.drozdetskiy.statistics.Statistics;
 import ru.cft.drozdetskiy.statistics.StatisticsFactory;
 
+/**
+ * Фабрика для создания объектов интерфейса {@link Statistics} параметризованных типами: {@link Long}, {@link Double}, {@link String}.
+ * Создаёт новые объекты для сбора краткой (SIMPLE) статистики.
+ */
 final public class SimpleStatisticsFactory implements StatisticsFactory {
 
     @Override
