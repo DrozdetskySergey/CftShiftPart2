5815eb926ec3cd1996b4932a4006997b70bd42aa
DrozdetskySergey <master@2867073.ru>
2025-05-10 18:07:19 +0700

    update StatisticsFactory javadoc

--- FilesFilter/src/ru/cft/drozdetskiy/statistics/StatisticsFactory.java
+++ FilesFilter/src/ru/cft/drozdetskiy/statistics/StatisticsFactory.java
@@ -1,7 +1,7 @@
 package ru.cft.drozdetskiy.statistics;
 
 /**
- * Абстрактная фабрика для создания объектов статистики {@link Statistics} определенных параметров.
+ * Абстрактная фабрика для создания объектов интерфейса {@link Statistics} параметризованных типами: {@link Long}, {@link Double}, {@link String}.
  * Предоставляет методы:
  * {@linkplain #createForLong() createForLong},
  * {@linkplain #createForDouble() createForDouble},
