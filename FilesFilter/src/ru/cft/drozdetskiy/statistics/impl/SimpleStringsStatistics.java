97f937afd95a65729cd883db12c1f4cc80c1126f
DrozdetskySergey <master@2867073.ru>
2025-05-10 18:22:49 +0700

    add SimpleStringsStatistics javadoc

--- FilesFilter/src/ru/cft/drozdetskiy/statistics/impl/SimpleStringsStatistics.java
+++ FilesFilter/src/ru/cft/drozdetskiy/statistics/impl/SimpleStringsStatistics.java
@@ -2,8 +2,14 @@ package ru.cft.drozdetskiy.statistics.impl;
 
 import ru.cft.drozdetskiy.statistics.Statistics;
 
+/**
+ * Краткая статистика по объектам {@link String}
+ */
 final class SimpleStringsStatistics implements Statistics<String> {
 
+    /**
+     * Количество.
+     */
     private long count = 0;
 
     @Override
