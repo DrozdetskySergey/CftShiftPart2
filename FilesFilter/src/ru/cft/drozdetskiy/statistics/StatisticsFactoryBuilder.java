67df58325b4d7f76b44c3f65e3ac4e04b56f2bef
DrozdetskySergey <master@2867073.ru>
2025-05-10 16:47:44 +0700

    add StatisticsFactoryBuilder javadoc

--- FilesFilter/src/ru/cft/drozdetskiy/statistics/StatisticsFactoryBuilder.java
+++ FilesFilter/src/ru/cft/drozdetskiy/statistics/StatisticsFactoryBuilder.java
@@ -3,8 +3,20 @@ package ru.cft.drozdetskiy.statistics;
 import ru.cft.drozdetskiy.statistics.impl.FullStatisticsFactory;
 import ru.cft.drozdetskiy.statistics.impl.SimpleStatisticsFactory;
 
+/**
+ * Функциональный класс. Предоставляет статичный метод {@linkplain #of(StatisticsType) of}
+ */
 public final class StatisticsFactoryBuilder {
 
+    private StatisticsFactoryBuilder() {
+    }
+
+    /**
+     * Создаёт объект интерфейса {@link StatisticsFactory} относительно переданного типа статистики.
+     *
+     * @param type требуемый тип статистики.
+     * @return объект конкретной фабрики реализующий интерфейс {@link StatisticsFactory}.
+     */
     public static StatisticsFactory of(StatisticsType type) {
         return type == StatisticsType.FULL ? new FullStatisticsFactory() : new SimpleStatisticsFactory();
     }
