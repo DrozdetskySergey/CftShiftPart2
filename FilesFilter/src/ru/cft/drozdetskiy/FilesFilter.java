76eb97714115095c7583d45ea48ffbf8f0b52b8f
DrozdetskySergey <master@2867073.ru>
2025-05-17 19:09:03 +0700

    update class FilesFilter: javadoc

--- FilesFilter/src/ru/cft/drozdetskiy/FilesFilter.java
+++ FilesFilter/src/ru/cft/drozdetskiy/FilesFilter.java
@@ -15,6 +15,15 @@ import java.util.Map;
 
 import static ru.cft.drozdetskiy.ContentType.*;
 
+/**
+ * Утилита фильтрации содержимого файлов. Задача: записать разные типы данных в разные файлы.
+ * При запуске в командной строке подается несколько файлов, содержащих целые числа,
+ * вещественные числа и строки. В качестве разделителя перевод строки.
+ * Строки из файлов читаются по очереди в соответствии с их перечислением в командной строке.
+ * Статистика по каждому типу выводится в консоль.
+ *
+ * @author Дроздецкий Сергей <a href="mailto:bigjunior@mail.ru">email</a>
+ */
 public final class FilesFilter {
 
     public static void main(String[] args) {
@@ -37,19 +46,28 @@ public final class FilesFilter {
                 System.out.println(allStatistics.get(DOUBLE));
                 System.out.println(allStatistics.get(STRING));
             } catch (NumberFormatException e) {
-                System.err.printf("Не верно распознана строка. %s%n", e.getMessage());
+                System.err.printf("Передайте эту информацию разработчику: %s%n", e.getMessage());
             } catch (IllegalArgumentException e) {
                 System.out.printf("Не верно задан аргумент: %s%n%n%s", e.getMessage(), help);
-            } catch (RuntimeException e) {
-                System.err.printf("Что-то пошло не так! %s%n", e.getMessage());
             } catch (IOException e) {
                 System.err.printf("Сбой файловой системой. %s%n", e.getMessage());
-            } catch (Exception e) {
+            } catch (RuntimeException e) {
+                System.err.println("Передайте эту информацию разработчику:");
                 e.printStackTrace();
             }
         }
     }
 
+    /**
+     * Центральный метод всей утилиты FilesFilter.
+     *
+     * @param dto DTO с вводными даннами для утилиты.
+     * @return неизменяемый словарь {@link Map} с собранной статистикой в соответствии с типом {@link ContentType}
+     * @throws IOException              если произошёл сбой при работе с файловой системой.
+     * @throws IllegalArgumentException если пользователь задал не верные данные.
+     * @throws NumberFormatException    если не удалось конвертировать строку в определённое число.
+     * @throws RuntimeException         если что-то пошло не так!
+     */
     private static Map<ContentType, Statistics<?>> filterFiles(ArgumentsDTO dto) throws IOException {
         createDirectory(Path.of(dto.getDirectory()).toAbsolutePath().normalize());
         Path longsFile = Path.of(dto.getDirectory(), dto.getPrefix() + "integers.txt").toAbsolutePath().normalize();
@@ -79,6 +97,12 @@ public final class FilesFilter {
         return allStatistics;
     }
 
+    /**
+     * Создаёт каталог по переданному пути.
+     *
+     * @param path путь для каталога.
+     * @throws IllegalArgumentException если не удалось создать каталог.
+     */
     private static void createDirectory(Path path) {
         try {
             Files.createDirectories(path);
@@ -87,12 +111,25 @@ public final class FilesFilter {
         }
     }
 
+    /**
+     * Бросает IllegalArgumentException если данный список путей к файлам содержит данный путь к файлу.
+     *
+     * @param files список путей к файлам.
+     * @param file  путь к файлу.
+     * @throws IllegalArgumentException если список путей содержит путь.
+     */
     private static void throwExceptionIfContains(List<Path> files, Path file) {
         if (files.contains(file)) {
             throw new IllegalArgumentException("имя файла совпадает с именем результата " + file);
         }
     }
 
+    /**
+     * Создаёт новый массив опций {@link StandardOpenOption} для открытия файла в зависимости от переданного флага.
+     *
+     * @param isAppend true если нужно открыть файл для записи в конец, false если нужно писать в начало файла.
+     * @return массив опций {@link StandardOpenOption}
+     */
     private static OpenOption[] getOpenOptions(boolean isAppend) {
         return isAppend ?
                 new StandardOpenOption[]{StandardOpenOption.CREATE, StandardOpenOption.APPEND} :
