e756f4b27d8d5e77c85b1dd32d8ebbecccfa4729
DrozdetskySergey <master@2867073.ru>
2025-10-30 17:37:48 +0700

    refact StatisticsFactory javadoc

--- src/main/java/ru/cft/drozdetskiy/statistics/StatisticsFactory.java
+++ src/main/java/ru/cft/drozdetskiy/statistics/StatisticsFactory.java
@@ -3,8 +3,7 @@ package ru.cft.drozdetskiy.statistics;
 import ru.cft.drozdetskiy.ContentType;
 
 /**
- * Абстрактная фабрика для создания объектов интерфейса {@link Statistics}
- * для разных значений определенного {@linkplain  ContentType типа}.
+ * Абстрактная фабрика для создания объектов интерфейса {@link Statistics} для всех значений {@linkplain  ContentType типа}.
  * Предоставляет методы:
  * {@linkplain #createForInteger() createForInteger},
  * {@linkplain #createForFloat() createForFloat},
@@ -13,21 +12,21 @@ import ru.cft.drozdetskiy.ContentType;
 public interface StatisticsFactory {
 
     /**
-     * Создаёт новый объект интерфейса {@link Statistics} для конкретного {@linkplain ContentType#INTEGER типа INTEGER}
+     * Создаёт новый объект интерфейса {@link Statistics} для типа {@linkplain ContentType#INTEGER INTEGER}
      *
      * @return {@link Statistics}
      */
     Statistics createForInteger();
 
     /**
-     * Создаёт новый объект интерфейса {@link Statistics} для конкретного {@linkplain ContentType#FLOAT типа FLOAT}
+     * Создаёт новый объект интерфейса {@link Statistics} для типа {@linkplain ContentType#FLOAT FLOAT}
      *
      * @return {@link Statistics}
      */
     Statistics createForFloat();
 
     /**
-     * Создаёт новый объект интерфейса {@link Statistics} для конкретного {@linkplain ContentType#STRING типа STRING}
+     * Создаёт новый объект интерфейса {@link Statistics} для типа {@linkplain ContentType#STRING STRING}
      *
      * @return {@link Statistics}
      */
