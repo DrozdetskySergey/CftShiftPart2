8334ea371476a40c7196f59a24af308c7ed57d82
DrozdetskySergey <master@2867073.ru>
2025-11-06 19:31:47 +0700

    add JUnit5 in project

---
+++ src/test/java/ru/cft/drozdetskiy/SeparatorTest.java
@@ -0,0 +1,10 @@
+package ru.cft.drozdetskiy;
+
+import org.junit.jupiter.api.Test;
+
+class SeparatorTest {
+
+    @Test
+    void handleStrings() {
+    }
+}
---
+++ src/test/java/ru/cft/drozdetskiy/args/ArgumentsTest.java
@@ -0,0 +1,10 @@
+package ru.cft.drozdetskiy.args;
+
+import org.junit.jupiter.api.Test;
+
+class ArgumentsTest {
+
+    @Test
+    void parse() {
+    }
+}
