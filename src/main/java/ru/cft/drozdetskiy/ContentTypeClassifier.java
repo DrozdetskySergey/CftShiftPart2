b6ef0fc5649d947f706ed6d0f515a99dae9c268f
DrozdetskySergey <master@2867073.ru>
2026-04-21 22:42:44 +0700

    extract ContentTypeClassifier class from Separator class; refact classify algorithm (no new arrays)

---
+++ src/main/java/ru/cft/drozdetskiy/ContentTypeClassifier.java
@@ -0,0 +1,89 @@
+package ru.cft.drozdetskiy;
+
+import static ru.cft.drozdetskiy.ContentType.*;
+
+/**
+ * Функциональный класс. Специализируется на определение {@linkplain  ContentType типа содержимого} строки.
+ * Реализует статичный метод {@linkplain #classify(String)}
+ */
+final class ContentTypeClassifier {
+
+    private ContentTypeClassifier() {
+    }
+
+    /**
+     * Определяет {@linkplain  ContentType тип содержимого} строки методом исключения.
+     * Сначала проверяет, что это целое число. Если нет, то проверяет, что это вещественное число.
+     * Если нет, тогда это обычная строка.
+     * У целого числа первый символ, кроме цифры, может быть плюс или минус, а остальные символы должны быть цифрами.
+     * У вещественного числа начало как у целого, а потом, при определённых условиях, должен встретиться
+     * один из трёх символов: [точка, e, E]. Так же возможно сочетание символа [точка] и ещё
+     * одного из двух символов: [e, E].
+     *
+     * @param string проверяемая строка.
+     * @return {@linkplain  ContentType Тип содержимого} строки.
+     */
+    public static ContentType classify(String string) {
+        if (string.isEmpty()) {
+            return STRING;
+        }
+
+        final int firstIndex = string.charAt(0) == '+' || string.charAt(0) == '-' ? 1 : 0;
+        final int lastIndex = string.length() - 1;
+        ContentType result = firstIndex <= lastIndex ? INTEGER : STRING; // STRING если строка "+" или "-"
+
+        for (int i = firstIndex; result != STRING && i <= lastIndex; i++) {
+            char symbol = string.charAt(i);
+
+            if (symbol == '.' && result == INTEGER && firstIndex != lastIndex) {
+                result = FLOAT;
+            } else if ((symbol == 'e' || symbol == 'E')
+                       && ((result == INTEGER && firstIndex < i) || (result == FLOAT && firstIndex + 1 < i))) {
+                result = isSubstringConvertedToInteger(string, i + 1) ? FLOAT : STRING;
+                break;
+            } else if (symbol < '0' || '9' < symbol) {
+                result = STRING;
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Проверяет что содержимое строки начиная с заданного индекса (подстрока) можно преобразовать в Integer
+     * исключая Integer.MIN_VALUE, то есть это целое число в диапазоне от -2147483647 до +2147483647.
+     * У целого числа первый символ, кроме цифры, может быть плюс или минус, а остальные символы должны быть цифрами.
+     *
+     * @param string     полная строка из которой берётся подстрока начиная с заданного индекса.
+     * @param beginIndex индекс начала подстроки
+     * @return true если содержимое строки можно конвертировать в Integer без Integer.MIN_VALUE.
+     */
+    private static boolean isSubstringConvertedToInteger(String string, int beginIndex) {
+        final int lastIndex = string.length() - 1;
+
+        if (lastIndex < beginIndex) {
+            return false;
+        }
+
+        int firstIndex = beginIndex + (string.charAt(beginIndex) == '+' || string.charAt(beginIndex) == '-' ? 1 : 0);
+
+        while (firstIndex < lastIndex && string.charAt(firstIndex) == '0') {
+            firstIndex++;
+        }
+
+        final int firstToLastIndexDiff = 9; // строка из 10 цифр имеет разницу крайних индексов 9 (последний - первый)
+        boolean result = firstIndex <= lastIndex && lastIndex <= firstIndex + firstToLastIndexDiff;
+
+        for (int i = firstIndex; result && i <= lastIndex; i++) {
+            char symbol = string.charAt(i);
+            result = '0' <= symbol && symbol <= '9';
+        }
+
+        if (result && lastIndex == firstIndex + firstToLastIndexDiff) {
+            String digits = string.substring(firstIndex);
+            result = digits.compareTo("2147483647") <= 0;
+        }
+
+        return result;
+    }
+}
--- src/main/java/ru/cft/drozdetskiy/Separator.java
+++ src/main/java/ru/cft/drozdetskiy/Separator.java
@@ -9,8 +9,6 @@ import java.util.EnumMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import static ru.cft.drozdetskiy.ContentType.*;
-
 /**
  * Распределяет строки в зависимости от {@linkplain  ContentType типа содержимого} по объектам
  * интерфейса {@link Appendable}, переданные в конструктор. При этом собирается статистика.
@@ -51,84 +49,11 @@ final class Separator {
 
         while (iterator.hasNext()) {
             String next = iterator.next();
-            ContentType type = classifyContentType(next);
+            ContentType type = ContentTypeClassifier.classify(next);
             writers.get(type).append(next).append(System.lineSeparator());
             allStatistics.get(type).include(next);
         }
 
         return Map.copyOf(allStatistics);
-    }
-
-    /**
-     * Определяет {@linkplain  ContentType тип содержимого} строки методом исключения.
-     * Сначала проверяет, что это целое число. Если нет, то проверяет, что это вещественное число.
-     * Если нет, тогда это простая строка.
-     * У целого числа первый символ, кроме цифры, может быть плюс или минус, а остальные символы должны быть цифрами.
-     * У вещественного числа начало как у целого, а потом, при определённых условиях, должен встретиться
-     * один из трёх символов: [точка, e, E]. Так же возможно сочетание символа [точка] и ещё
-     * одного из двух символов: [e, E].
-     *
-     * @param string проверяемая строка.
-     * @return {@linkplain  ContentType Тип содержимого} строки.
-     */
-    private ContentType classifyContentType(String string) {
-        if (string.isEmpty()) {
-            return STRING;
-        }
-
-        final char[] symbols = string.toCharArray();
-        final int firstIndex = symbols[0] == '+' || symbols[0] == '-' ? 1 : 0;
-        final int lastIndex = symbols.length - 1;
-        ContentType result = firstIndex <= lastIndex ? INTEGER : STRING; // STRING если строка "+" или "-"
-
-        for (int i = firstIndex; result != STRING && i <= lastIndex; i++) {
-            if (symbols[i] == '.' && result == INTEGER && firstIndex != lastIndex) {
-                result = FLOAT;
-            } else if ((symbols[i] == 'e' || symbols[i] == 'E')
-                       && ((result == INTEGER && firstIndex < i) || (result == FLOAT && firstIndex + 1 < i))) {
-                result = isConvertedToInteger(string.substring(i + 1)) ? FLOAT : STRING;
-                break;
-            } else if (symbols[i] < '0' || '9' < symbols[i]) {
-                result = STRING;
-            }
-        }
-
-        return result;
-    }
-
-    /**
-     * Проверяет что содержимое строки можно преобразовать в Integer исключая Integer.MIN_VALUE,
-     * то есть это целое число в диапазоне от -2147483647 до +2147483647.
-     * У целого числа первый символ, кроме цифры, может быть плюс или минус, а остальные символы должны быть цифрами.
-     *
-     * @param string проверяемая строка.
-     * @return true если содержимое строки можно конвертировать в Integer без Integer.MIN_VALUE.
-     */
-    private boolean isConvertedToInteger(String string) {
-        if (string.isEmpty()) {
-            return false;
-        }
-
-        final char[] symbols = string.toCharArray();
-        int firstIndex = symbols[0] == '+' || symbols[0] == '-' ? 1 : 0;
-        final int lastIndex = symbols.length - 1;
-
-        while (firstIndex < lastIndex && symbols[firstIndex] == '0') {
-            firstIndex++;
-        }
-
-        final int firstToLastIndexDiff = 9; // строка из 10 цифр имеет разницу крайних индексов 9 (последний - первый)
-        boolean result = firstIndex <= lastIndex && lastIndex <= firstIndex + firstToLastIndexDiff;
-
-        for (int i = firstIndex; result && i <= lastIndex; i++) {
-            result = '0' <= symbols[i] && symbols[i] <= '9';
-        }
-
-        if (result && lastIndex == firstIndex + firstToLastIndexDiff) {
-            String digits = string.substring(firstIndex);
-            result = digits.compareTo("2147483647") <= 0;
-        }
-
-        return result;
     }
 }
