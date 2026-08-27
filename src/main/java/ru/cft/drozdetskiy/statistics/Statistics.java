8f6dd798446cc85e24fbf64d560a5a52ba0a856f
DrozdetskySergey <master@2867073.ru>
2026-08-27 19:05:17 +0700

    update interface Statistics to abstract class (abstract metods: include, getAdditionalInfo)

--- src/main/java/ru/cft/drozdetskiy/statistics/Statistics.java
+++ src/main/java/ru/cft/drozdetskiy/statistics/Statistics.java
@@ -7,16 +7,26 @@ import ru.cft.drozdetskiy.ContentType;
  * при создании этого объекта. Предоставляет методы: {@linkplain #include(String)}, {@linkplain #getContentType()},
  * {@linkplain #getCount()}, {@linkplain #getAdditionalInfo()}.
  */
-public interface Statistics {
+public abstract class Statistics {
 
     /**
-     * Статистически обрабатывает значение переданное в формате строки и добавляет нужную информацию в данную статистику.
-     * Ожидаемый тип значения (содержимое переданной строки) отдаёт метод {@linkplain #getContentType()}
-     *
-     * @param value значение в виде строки о котором нужно добавить информацию в статистику.
-     * @throws NumberFormatException если статистика обрабатывает числовые значения, и переданное значение невалидное.
+     * Тип содержимого строки, которая ожидается в качестве параметра в методе {@linkplain #include(String)}.
      */
-    void include(String value);
+    private final ContentType contentType;
+
+    /**
+     * Количество значений которые были переданы в данную статистику через метод {@linkplain #include(String)}
+     * и удачно обработаны.
+     */
+    private long count;
+
+    /**
+     * Статистика о значениях переданных в формате строки. {@linkplain ContentType Тип содержимого} строк фиксируется
+     * при создании этого объекта.
+     */
+    public Statistics(ContentType contentType) {
+        this.contentType = contentType;
+    }
 
     /**
      * Отдаёт {@linkplain ContentType тип содержимого} строки, которая ожидается в качестве параметра у метода
@@ -24,7 +34,9 @@ public interface Statistics {
      *
      * @return тип содержимого строки.
      */
-    ContentType getContentType();
+    public ContentType getContentType() {
+        return contentType;
+    }
 
     /**
      * Отдаёт количество значений которые были переданы в данную статистику через метод {@linkplain #include(String)}
@@ -32,12 +44,23 @@ public interface Statistics {
      *
      * @return количество статистически обработанных значений.
      */
-    long getCount();
+    public long getCount() {
+        return count;
+    }
+
+    /**
+     * Статистически обрабатывает значение переданное в формате строки и добавляет нужную информацию в данную статистику.
+     * Ожидаемый тип значения (содержимое переданной строки) отдаёт метод {@linkplain #getContentType()}
+     *
+     * @param value значение в формате строки о котором нужно добавить информацию в статистику.
+     * @throws NumberFormatException если статистика обрабатывает числовые значения, и переданное значение невалидное.
+     */
+    public abstract void include(String value);
 
     /**
      * Отдаёт строку в которой содержится вся дополнительная информация из данной статистики в удобочитаемом виде.
      *
      * @return дополнительная информация в виде строки.
      */
-    String getAdditionalInfo();
+    public abstract String getAdditionalInfo();
 }
