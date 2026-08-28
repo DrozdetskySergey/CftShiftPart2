350491e9c846952412c3842bd664863b9d673c27
DrozdetskySergey <master@2867073.ru>
2026-08-28 16:00:42 +0700

    update javadoc + refact (rename) Statistics.java

--- src/main/java/ru/cft/drozdetskiy/statistics/Statistics.java
+++ src/main/java/ru/cft/drozdetskiy/statistics/Statistics.java
@@ -3,39 +3,40 @@ package ru.cft.drozdetskiy.statistics;
 import ru.cft.drozdetskiy.ContentType;
 
 /**
- * Статистика о значениях переданных в формате строки. {@linkplain ContentType Тип содержимого} строк фиксируется
+ * Статистика о значениях переданных в формате строки. {@linkplain ContentType Тип содержимого} строки фиксируется
  * при создании этого объекта. Предоставляет методы: {@linkplain #include(String)}, {@linkplain #getContentType()},
  * {@linkplain #getCount()}, {@linkplain #getAdditionalInfo()}.
  */
 public abstract class Statistics {
 
     /**
-     * Тип содержимого строки, которая ожидается в качестве параметра в методе {@linkplain #include(String)}.
+     * Тип содержимого строки, которая ожидается в качестве параметра для метода {@linkplain #include(String)}.
      */
-    private final ContentType contentType;
-
+    private final ContentType type;
     /**
-     * Количество значений которые были переданы в данную статистику через метод {@linkplain #include(String)}
+     * Счётчик количества значений которые были переданы в данную статистику через метод {@linkplain #include(String)}
      * и удачно обработаны.
      */
     protected long count;
 
     /**
-     * Статистика о значениях переданных в формате строки. {@linkplain ContentType Тип содержимого} строк фиксируется
+     * Статистика о значениях переданных в формате строки. {@linkplain ContentType Тип содержимого} строки фиксируется
      * при создании этого объекта.
+     *
+     * @param type Тип содержимого строки.
      */
-    public Statistics(ContentType contentType) {
-        this.contentType = contentType;
+    public Statistics(ContentType type) {
+        this.type = type;
     }
 
     /**
-     * Отдаёт {@linkplain ContentType тип содержимого} строки, которая ожидается в качестве параметра у метода
-     * {@linkplain #include(String)}. Он определяется при создании этого объекта статистики и никогда не меняется.
+     * Отдаёт {@linkplain ContentType тип содержимого} строки, которая ожидается в качестве параметра для метода
+     * {@linkplain #include(String)}. Он фиксируется при создании этого объекта статистики.
      *
      * @return тип содержимого строки.
      */
     public ContentType getContentType() {
-        return contentType;
+        return type;
     }
 
     /**
@@ -50,7 +51,7 @@ public abstract class Statistics {
 
     /**
      * Статистически обрабатывает значение переданное в формате строки и добавляет нужную информацию в данную статистику.
-     * Ожидаемый тип значения (содержимое переданной строки) отдаёт метод {@linkplain #getContentType()}
+     * Ожидаемый тип значения (тип содержимого переданной строки) отдаёт метод {@linkplain #getContentType()}
      *
      * @param value значение в формате строки о котором нужно добавить информацию в статистику.
      * @throws NumberFormatException если статистика обрабатывает числовые значения, и переданное значение невалидное.
@@ -58,7 +59,7 @@ public abstract class Statistics {
     public abstract void include(String value);
 
     /**
-     * Отдаёт строку в которой содержится вся дополнительная информация из данной статистики в удобочитаемом виде.
+     * Отдаёт строку в которой содержится вся дополнительная информация из данной статистики желательно в удобочитаемом виде.
      *
      * @return дополнительная информация в виде строки.
      */
